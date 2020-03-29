e3031d87ec54483467ad2786641c8704b21ca829
Peter Stanko <wermington@gmail.com>
2020-03-29 19:45:21 +0200

    Additional columns - date and time

--- core/csv.go
+++ core/csv.go
@@ -12,6 +12,8 @@ type CSVStatistic struct {
 	StudentID string  `csv:"student_id"`
 	Index     int     `json:"index" csv:"index"`
 	DateTime  string  `csv:"datetime"`
+	Date      string  `csv:"date"`
+	Time      string  `csv:"time"`
 	Points    float64 `json:"points" csv:"points"`
 	Bonus     float64 `json:"bonus" csv:"bonus"`
 	Final     bool    `json:"final" csv:"final"`
@@ -33,12 +35,15 @@ func WriteStatisticsToCSVFile(file string, statistics []CSVStatistic) error {
 // ConvertSubmissionsToCSVStatistics - Converter
 func ConvertSubmissionsToCSVStatistics(students []StudentInfo) []CSVStatistic {
 	var stats []CSVStatistic
+
 	for _, student := range students {
 		for _, submission := range student.Submissions {
 			stat := CSVStatistic{
 				StudentID: student.ID.String(),
 				Index:     submission.Index,
-				DateTime:  submission.DateTime.String(),
+				DateTime:  submission.DateTime.Format("2006-01-02T15:04"),
+				Date:      submission.DateTime.Format("2006-01-02"),
+				Time:      submission.DateTime.Format("15-04"),
 				Points:    submission.Points,
 				Final:     submission.Final,
 				Bonus:     submission.Bonus,
@@ -46,5 +51,6 @@ func ConvertSubmissionsToCSVStatistics(students []StudentInfo) []CSVStatistic {
 			stats = append(stats, stat)
 		}
 	}
+
 	return stats
 }
